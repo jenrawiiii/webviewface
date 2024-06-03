6c6ef3a94f2ed210475360d08039a19cb96bd94a
Jenrawiiii <jenrawee.c@ku.th>
2024-06-03 15:15:32 +0700

    update

--- src/app/dialog/dialog-estimate/dialog-estimate.component.ts
+++ src/app/dialog/dialog-estimate/dialog-estimate.component.ts
@@ -34,13 +34,27 @@ export class DialogEstimateComponent {
       psid: '7550857141677622'  // คุณต้องหา PSID ของผู้ใช้จากที่ไหนสักที่ เช่น ผ่านการเข้าสู่ระบบหรือเก็บไว้ในแอปพลิเคชันของคุณ
     };
 
-    this.http.post('http://localhost:3000/submit-survey', surveyData)
+    this.http.post('https://be9a-171-97-97-20.ngrok-free.app/submit-survey', surveyData)
       .subscribe(response => {
-        console.log('Survey submitted successfully');
+        console.log('Survey submitted successfully', response);
+        
       }, error => {
         console.error('Error submitting survey', error);
       });
 
     this.isDialogOpen = false; // ปิด dialog หลังจากส่งฟอร์มเรียบร้อย
   }
+
+  sendConfirmationMessage(psid: string, message: string) {
+    const PAGE_ACCESS_TOKEN = 'EAAJpygIudTYBOZBJ1UD6Dq4kjDx6UnfmJIER1C1OceT9SUdkD6vw0BU2PTM4jBIgSEGqD7kf7v6gcizFuWiMM69RuZBfqWmjHoNWPlDsvwbUJxqZBAzcZBZCTi7ukRESi0GLwawZAZCZCpuG57M8mtkMWa57IKAUcKrPyOffeTsEZC7h07ZCJzp1fE0ZC2G4JAGA2MS';
+    const url = `https://graph.facebook.com/v20.0/me/messages?access_token=${PAGE_ACCESS_TOKEN}`;
+    const body = {
+      recipient: { id: psid },
+      message: { text: message }
+    };
+
+    console.log('Sending confirmation message with payload:', body);
+    return this.http.post(url, body);
+  }
 }
+
